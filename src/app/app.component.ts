c7efe89d1673405f5fbdc9050abdfb41d8bf032d
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-06 17:08:07 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,26 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { BreakpointObserver } from "@angular/cdk/layout";
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-your-component',
+  templateUrl: './your-component.component.html',
+  styleUrls: ['./your-component.component.scss']
 })
-export class AppComponent implements AfterViewInit {
-  title = 'material-tailwindl-angular-project';
-  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+export class YourComponent implements AfterViewInit {
+  @ViewChild('sidenav') sideNav!: MatSidenav;
 
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef) { }
 
-  constructor (private observer : BreakpointObserver) {
-
-  }
   ngAfterViewInit(): void {
-    // throw new Error('Method not implemented.');
-    this.sideNav.opened = true;
-    this.observer.observe(['(max-width:787px)'])
-    .subscribe((res)=> {
-      if(res?.matches){
+    this.observer.observe(['(max-width:787px)']).subscribe((res) => {
+      if (res?.matches) {
         this.sideNav.mode = "over";
         this.sideNav.close();
-      }else {
+      } else {
         this.sideNav.mode = "side";
         this.sideNav.open();
       }
-    })
+    });
   }
 }
