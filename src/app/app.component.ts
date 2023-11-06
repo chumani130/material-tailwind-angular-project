a44d7e3a424632df84d7da60c15562a2b35f6afa
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-06 10:54:31 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'material-tailwindl-angular-project';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
+
+
+  constructor (private observer : BreakpointObserver) {
+
+  }
+  ngAfterViewInit(): void {
+    // throw new Error('Method not implemented.');
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:787px)'])
+    .subscribe((res)=> {
+      if(res?.matches){
+        this.sideNav.mode = "over";
+        this.sideNav.close();
+      }else {
+        this.sideNav.mode = "side";
+        this.sideNav.open();
+      }
+    })
+  }
 }
